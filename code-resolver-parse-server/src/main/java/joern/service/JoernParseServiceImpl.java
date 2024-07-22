726b19cc8f9cd277eb9d12e6f25b76570f2c9310
Whtbear <895702850@qq.com>
2024-07-22 09:59:07 +0800

    record 7/22 9:56         修改了路径导致的bug，并对out加了删除命令，i++的判断条件设置为4

--- code-resolver-parse-server/src/main/java/joern/service/JoernParseServiceImpl.java
+++ code-resolver-parse-server/src/main/java/joern/service/JoernParseServiceImpl.java
@@ -31,15 +31,17 @@ public class JoernParseServiceImpl {
         int i=0;
         System.setProperty("joern",joern_dir);
         System.setProperty("import_db",toDb_dir);
+        commands.add("rmdir " + joern_dir + "\\out" );//这里需要先把out删了再后面创建
         commands.add("joern-parse "+url+" -o "+cpg_dir);
-        commands.add("joern-export.bat --repr=all --format=neo4jcsv "+cpg_dir);
-        commands.add("move "+source_dir+" "+import_dir);
-        commands.add("for %f in ("+node_dir+") do bin\\cypher-shell -u neo4j -p 19990528 -d mvc --file %f");
-        commands.add("for %f in ("+edge_dir+") do bin\\cypher-shell -u neo4j -p 19990528 -d mvc --file %f");
+        commands.add("joern-export.bat --repr=all --format=neo4jcsv "+cpg_dir);//这里好像joern里如果存在joern_dir则不会覆盖
+        commands.add("move  "+source_dir+" "+import_dir);//删除joern\out后可正常运行
+        System.out.println("source:" + source_dir + " ////  import_dir:" + import_dir);
+        commands.add("for %f in ("+node_dir+") do bin\\cypher-shell -u neo4j -p chenqiujian123 -d mvc --file %f");
+        commands.add("for %f in ("+edge_dir+") do bin\\cypher-shell -u neo4j -p chenqiujian123 -d mvc --file %f");
         for (String command : commands) {
             ProcessBuilder builder = new ProcessBuilder();
             builder.command("cmd.exe", "/c",command);
-            builder.directory(i++<3?new File(System.getProperty("joern")):new File(System.getProperty("import_db")));
+            builder.directory(i++<4?new File(System.getProperty("joern")):new File(System.getProperty("import_db")));
             try{
                 Process process=builder.start();
                 StreamGobbler streamGobbler =
